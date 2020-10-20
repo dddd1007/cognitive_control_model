82ee11ffa04a69966f2d2653750147c40d886bc8
Xiaokai Xia <xia@xiaokai.me>
2020-10-20 09:44:19 +0800

    format code

--- models/src/RLModels/RLModels.jl
+++ models/src/RLModels/RLModels.jl
@@ -163,13 +163,9 @@ end
 # 定义更新价值矩阵的函数
 
 # 具体SR联结学习的价值更新函数
-function update_options_weight_matrix(
-    weight_vector::Array{Float64,1},
-    α::Float64,
-    decay::Float64,
-    correct_selection::Tuple;
-    dodecay = true, debug = false)
-    
+function update_options_weight_matrix(weight_vector::Array{Float64,1}, α::Float64,
+                                      decay::Float64, correct_selection::Tuple;
+                                      dodecay=true, debug=false)
     weight_matrix = reshape(weight_vector, 2, 2)'
     correct_selection_idx = CartesianIndex(correct_selection) + CartesianIndex(1, 1)
     op_selection_idx = abs(correct_selection[1] - 1) + 1
@@ -177,16 +173,17 @@ function update_options_weight_matrix(
     if debug
         println("True selection is " * repr(correct_selection_idx))
         println("The value is " * repr(weight_matrix[correct_selection_idx]))
+        println("True selection is " * repr(correct_selection_idx))
+        println("The value is " * repr(weight_matrix[correct_selection_idx]))
     end
 
-    weight_matrix[correct_selection_idx] =
-        weight_matrix[correct_selection_idx] +
-        α * (1 - weight_matrix[correct_selection_idx])
+    weight_matrix[correct_selection_idx] = weight_matrix[correct_selection_idx] +
+                                           α * (1 - weight_matrix[correct_selection_idx])
 
     if dodecay
-        weight_matrix[op_selection_idx, :] =
-            weight_matrix[op_selection_idx, :] .+
-            decay .* (0.5 .- weight_matrix[op_selection_idx, :])
+        weight_matrix[op_selection_idx, :] = weight_matrix[op_selection_idx, :] .+
+                                             decay .*
+                                             (0.5 .- weight_matrix[op_selection_idx, :])
     end
 
     return reshape(weight_matrix', 1, 4)
