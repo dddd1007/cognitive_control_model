00cfb291e567124f7e561ad7b0a9ff6de92efbf9
Xiaokai Xia <xia@xiaokai.me>
2020-11-18 10:19:24 +0800

    探索正的loglikelihood的原因

--- notebooks/read_eval_result2.jl
+++ notebooks/read_eval_result2.jl
@@ -7,6 +7,12 @@ using InteractiveUtils
 # ╔═╡ b7211716-27d6-11eb-0baa-e5791b5954ee
 using JLD2, Models
 
+# ╔═╡ c97206aa-27e0-11eb-08b0-71e374f93c88
+using DataFrames
+
+# ╔═╡ 8eea045a-27e1-11eb-21e4-8b71eaff7541
+using CSV
+
 # ╔═╡ 2b61fe22-27d7-11eb-1a33-194ac35b816b
 @load "/Users/dddd1007/project2git/cognitive_control_model/data/output/RLModels/model_selection/2020-11-12-065333.jld2" eval_results
 
@@ -20,10 +26,38 @@ md"## 获取关于结果的表格"
 subject_names = keys(eval_results)
 
 # ╔═╡ dd6137d8-27d8-11eb-27b0-190c68ba8a88
-query_result = eval_results['sub25_LuSihan']['single_alpha']
+query_result = eval_results["sub25_LuSihan"]
 
 # ╔═╡ 601d0ccc-27d9-11eb-2551-0feb4f859f46
+function show_aic(eval_results, index)
+	[eval_results.single_alpha[:eval_result][index],
+		eval_results.single_alpha_no_decay[:eval_result][index],
+		eval_results.no_decay[:eval_result][index],
+		eval_results.basic[:eval_result][index],
+		eval_results.error[:eval_result][index],
+		eval_results.CCC_same_alpha[:eval_result][index],
+		eval_results.CCC_different_alpha[:eval_result][index]]
+end
 
+# ╔═╡ 3b0f2ec4-27e0-11eb-244c-6709a228f931
+foo = []
+
+# ╔═╡ 0164e7ec-27e3-11eb-1406-fb26c1ef9ac6
+show_aic(eval_results["sub31_TanPeixian"], :MSE)
+
+# ╔═╡ f6530bbc-27de-11eb-10ac-a1e9de05c68f
+for i in collect(subject_names)
+	push!(foo, [i, show_aic(eval_results[i], :MSE)...])
+end
+
+# ╔═╡ 9becefc2-27e2-11eb-1f9c-1376fe2caf1f
+#foo
+
+# ╔═╡ f1ac02ae-27e0-11eb-03e5-9d2614ffd5b9
+foo2 = DataFrame(foo)
+
+# ╔═╡ bf923aa0-27e1-11eb-1937-69d27fd159da
+CSV.write("/Users/dddd1007/Dropbox/My Mac (eXrld-MBP)/Desktop/test.csv", foo2)
 
 # ╔═╡ Cell order:
 # ╠═b7211716-27d6-11eb-0baa-e5791b5954ee
@@ -33,3 +67,11 @@ query_result = eval_results['sub25_LuSihan']['single_alpha']
 # ╠═dd8b42d6-27d7-11eb-337d-2f5ab764aad8
 # ╠═dd6137d8-27d8-11eb-27b0-190c68ba8a88
 # ╠═601d0ccc-27d9-11eb-2551-0feb4f859f46
+# ╠═c97206aa-27e0-11eb-08b0-71e374f93c88
+# ╠═3b0f2ec4-27e0-11eb-244c-6709a228f931
+# ╠═0164e7ec-27e3-11eb-1406-fb26c1ef9ac6
+# ╠═f6530bbc-27de-11eb-10ac-a1e9de05c68f
+# ╠═9becefc2-27e2-11eb-1f9c-1376fe2caf1f
+# ╠═f1ac02ae-27e0-11eb-03e5-9d2614ffd5b9
+# ╠═8eea045a-27e1-11eb-21e4-8b71eaff7541
+# ╠═bf923aa0-27e1-11eb-1937-69d27fd159da
--- ref_code/Lingwang_CCC/model_selection.m
+++ ref_code/Lingwang_CCC/model_selection.m
@@ -1,25 +1,20 @@
-function model_selection(filename)
+%function model_selection(filename)
 
-if ispc
-    addpath('X:\Toolbox\SPM12')
-elseif isunix
-    addpath('/Data/Toolbox/SPM12')
-end
-
-results_folder = './model_results/';
+results_folder = "/Users/dddd1007/project2git/cognitive_control_model/data/output/summary/"
+filename = "2nd_model_selection_aic.csv"
 
 %AIC
-data = csvread([results_folder filename],1,1);
+data = csvread(append(results_folder,filename),1,1);
 data = -1 * data;
 [alpha,exp_r,xp,pxp,bor] = spm_BMS(data);
 
 %wirte results
-fid = fopen([results_folder filename],'r');
+fid = fopen(append(results_folder,filename),'r');
 header = textscan(fid,'%s', 1, 'Delimiter','\n', 'headerlines', 0);
 header{1}{1}(1:7) = [];
 fclose(fid);
 
-fid = fopen([results_folder 'model_selection_' filename], 'w');
+fid = fopen(append(results_folder,'model_selection_',filename), 'w');
 fprintf(fid, '%s', header{1}{1});
 fprintf(fid, '\n%s,','alpha');fprintf(fid, '%f,', alpha);
 fprintf(fid, '\n%s,','exp_r');fprintf(fid, '%f,', exp_r);
