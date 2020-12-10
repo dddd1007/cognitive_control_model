54fcdfac4266688b56cf498a2d6dcae6ee9f348c
Xiaokai Xia <xia@xiaokai.me>
2020-12-10 16:37:44 +0800

    修改模型选择脚本

--- ref_code/Lingwang_CCC/model_selection.m
+++ ref_code/Lingwang_CCC/model_selection.m
@@ -1,20 +1,15 @@
-%function model_selection(filename)
-
-results_folder = "/Users/dddd1007/project2git/cognitive_control_model/data/output/summary/"
-filename = "2nd_model_selection_aic.csv"
-
 %AIC
-data = csvread(append(results_folder,filename),1,1);
+data = csvread(append("/Users/dddd1007/project2git/cognitive_control_model/data/output/RLModels/model_selection/model_selection_final.csv"),1,1);
 data = -1 * data;
 [alpha,exp_r,xp,pxp,bor] = spm_BMS(data);
 
 %wirte results
-fid = fopen(append(results_folder,filename),'r');
+fid = fopen(append("/Users/dddd1007/project2git/cognitive_control_model/data/output/RLModels/model_selection/model_selection_final.csv"),'r');
 header = textscan(fid,'%s', 1, 'Delimiter','\n', 'headerlines', 0);
 header{1}{1}(1:7) = [];
 fclose(fid);
 
-fid = fopen(append(results_folder,'model_selection_',filename), 'w');
+fid = fopen(append("/Users/dddd1007/project2git/cognitive_control_model/data/output/RLModels/model_selection/model_selection_final_result.csv"), 'w');
 fprintf(fid, '%s', header{1}{1});
 fprintf(fid, '\n%s,','alpha');fprintf(fid, '%f,', alpha);
 fprintf(fid, '\n%s,','exp_r');fprintf(fid, '%f,', exp_r);
