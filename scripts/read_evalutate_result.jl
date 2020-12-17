3750cf3a6d30f72251e4975dc3918bd5f0d1fa2d
Xiaokai Xia <xia@xiaokai.me>
2020-12-17 16:52:24 +0800

    读取和生成带有参数的数据表格

--- scripts/read_evalutate_result.jl
+++ scripts/read_evalutate_result.jl
@@ -1,4 +1,4 @@
-using JLD2, DataFrames, FileIO, CSV
+using JLD2, DataFrames, DataFramesMeta, FileIO, CSV
 
 @load "/Users/dddd1007/project2git/cognitive_control_model/data/output/RLModels/model_selection/2020-12-09-155938final_selection.jld2"
 
@@ -17,3 +17,32 @@ for i in values(eval_results)
 end
 
 CSV.write("/Users/dddd1007/Dropbox/My Mac (eXrld-MBP)/Desktop/model_selection_final.csv", DataTable)
+
+Q_value_table = DataFrame(subject = [], _1a = [], _1a1d = [], _1a1d1e = [], _1a1d1CCC = [], _1a1d1e1CCC = [], _2a = [], _2a1d = [], _2a1d1e = [], _2a1d1CCC = [], _2a1d1e1CCC = [])
+for i in values(eval_results)
+    result = []
+	push!(result, i[1])
+	
+	for j in 2:11
+        push!(result, i[j][:p_history])
+	end
+	
+    push!(Q_value_table, result)
+end
+
+_2a1d1CCC = sort!(select(Q_value_table, :subject, :_2a1d1CCC))
+
+pure_all_data = CSV.read("/Users/dddd1007/project2git/cognitive_control_model/data/input/pure_all_data.csv", DataFrame)
+_2a1d1CCC_p = Array{Float64,1}(undef, nrow(pure_all_data))
+pure_all_data[!, :_2a1d1CCC] = _2a1d1CCC_p
+
+new_dataframe = @where(pure_all_data, :Subject .== 37)
+for i in _2a1d1CCC[!, :subject]
+    input_dataframe = @where(_2a1d1CCC, :subject .== i)
+    foo = @where(pure_all_data, :Subject .== i, :Type .!= "miss")
+    println(length(input_dataframe._2a1d1CCC[1]))
+    foo._2a1d1CCC = input_dataframe._2a1d1CCC[1]
+    new_dataframe = vcat(new_dataframe, foo)
+end
+
+CSV.write("/Users/dddd1007/project2git/cognitive_control_model/data/input/all_data_with_2a1d1CCC.csv", new_dataframe)
