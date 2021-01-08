9c01534a46205ae65203537baf2e96c5401ba4ba
Xiaokai Xia <xia@xiaokai.me>
2021-01-08 09:39:05 +0800

    读取估计完的数据并导出用于下一步分析

--- scripts/read_evalutate_result.jl
+++ scripts/read_evalutate_result.jl
@@ -1,6 +1,6 @@
 using JLD2, DataFrames, DataFramesMeta, FileIO, CSV
 
-@load "/Users/dddd1007/project2git/cognitive_control_model/data/output/RLModels/model_selection/2020-12-09-155938final_selection.jld2"
+@load "/Users/dddd1007/project2git/cognitive_control_model/data/output/RLModels/model_selection/2021-01-07-145437final_selection_correct_CCC.jld2"
 
 DataTable = DataFrame(subject = [], _1a = [], _1a1d = [], _1a1d1e = [], _1a1d1CCC = [], _1a1d1e1CCC = [], _2a = [], _2a1d = [], _2a1d1e = [], _2a1d1CCC = [], _2a1d1e1CCC = [])
 subject_names = collect(keys(eval_results))
@@ -9,14 +9,14 @@ for i in values(eval_results)
     result = []
 	push!(result, i[1])
 	
-	for j in 2:11i
+	for j in 2:11
         push!(result, i[j][:eval_result][:AIC])
 	end
 	
     push!(DataTable, result)
 end
 
-CSV.write("/Users/dddd1007/Dropbox/My Mac (eXrld-MBP)/Desktop/model_selection_final.csv", DataTable)
+CSV.write("/Users/dddd1007/project2git/cognitive_control_model/data/output/summary/model_selection_correct_CCC.csv", DataTable)
 
 Q_value_table = DataFrame(subject = [], _1a = [], _1a1d = [], _1a1d1e = [], _1a1d1CCC = [], _1a1d1e1CCC = [], _2a = [], _2a1d = [], _2a1d1e = [], _2a1d1CCC = [], _2a1d1e1CCC = [])
 for i in values(eval_results)
@@ -31,18 +31,22 @@ for i in values(eval_results)
 end
 
 _2a1d1CCC = sort!(select(Q_value_table, :subject, :_2a1d1CCC))
+_2a1d1e = sort!(select(Q_value_table, :subject, :_2a1d1e))
 
 pure_all_data = CSV.read("/Users/dddd1007/project2git/cognitive_control_model/data/input/pure_all_data.csv", DataFrame)
 _2a1d1CCC_p = Array{Float64,1}(undef, nrow(pure_all_data))
 pure_all_data[!, :_2a1d1CCC] = _2a1d1CCC_p
+_2a1d1e_p = Array{Float64,1}(undef, nrow(pure_all_data))
+pure_all_data[!, :_2a1d1e] = _2a1d1e_p
 
 new_dataframe = @where(pure_all_data, :Subject .== 37)
 for i in _2a1d1CCC[!, :subject]
-    input_dataframe = @where(_2a1d1CCC, :subject .== i)
+    input_dataframe1 = @where(_2a1d1CCC, :subject .== i)
+    input_dataframe2 = @where(_2a1d1e,   :subject .== i)
     foo = @where(pure_all_data, :Subject .== i, :Type .!= "miss")
-    println(length(input_dataframe._2a1d1CCC[1]))
-    foo._2a1d1CCC = input_dataframe._2a1d1CCC[1]
+    foo._2a1d1CCC = input_dataframe1._2a1d1CCC[1]
+    foo._2a1d1e = input_dataframe2._2a1d1e[1]
     new_dataframe = vcat(new_dataframe, foo)
 end
 
-CSV.write("/Users/dddd1007/project2git/cognitive_control_model/data/input/all_data_with_2a1d1CCC.csv", new_dataframe)
+CSV.write("/Users/dddd1007/project2git/cognitive_control_model/data/input/all_data_with_2a1d1CCC_2a1d1e.csv", new_dataframe)
