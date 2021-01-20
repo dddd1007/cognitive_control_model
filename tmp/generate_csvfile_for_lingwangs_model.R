9abd2cc1faff316fca982ac103836c2ac46c605a
Xiaokai Xia <xia@xiaokai.me>
2021-01-20 11:23:26 +0800

    修改RLCC模型代码, 和老师的文件保持一致

--- tmp/generate_csvfile_for_lingwangs_model.R
+++ tmp/generate_csvfile_for_lingwangs_model.R
@@ -7,7 +7,7 @@ renamer <- function(input_dataframe, baseline_margin){
     input_dataframe <- select(input_dataframe, stim_loc,Type,Response,RT, congruency, run, Trial, correct_action)
     input_dataframe <- input_dataframe %>%
         mutate(error_outlier = abs(Type - 1)) %>%
-        rename(stimPosit = stim_loc, correction = Type, respPosit = correct_action, nblock = run, hand = Response)
+        rename(stimPosit = stim_loc, correction = Type, respPosit = correct_action, nblock = run, hand = Response, trial_index = Trial)
     postError <- c(0, input_dataframe$error_outlier)
     postError <- postError[-length(postError)]
     baseline_margin = baseline_margin[1:nrow(input_dataframe),]
@@ -18,7 +18,7 @@ renamer <- function(input_dataframe, baseline_margin){
 }
 
 savepath = "/Users/dddd1007/project2git/cognitive_control_model/ref_code/Lingwang_CCC/data/"
-for (i in 28:36) {
+for (i in 1:36) {
     foo = filter(all_data, Subject_num == i)
     bar = renamer(foo, baseline_margin)
     write_csv(bar, paste0(savepath, "sub_", i, "_prepared_data.csv"))
