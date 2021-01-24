557d0329e8839d4b4de91b7d786b440f7e4f3ccc
Xiaokai Xia <xia@xiaokai.me>
2021-01-24 14:23:18 +0800

    修改提供给老师的数据的生成脚本

--- scripts/dicover_gradient_of_model.jl
+++ scripts/dicover_gradient_of_model.jl
@@ -1,1 +1,25 @@
 using Models, DataFrames, Hyperopt
+
+include("/Users/dddd1007/project2git/cognitive_control_model/tmp/init_sub1_data.jl")
+
+v_list = []
+s_list = []
+mse_list = []
+
+ho = @hyperopt for i = 10000,
+        α_v = [0.001:0.001:1;],
+        α_s = [0.001:0.001:1;]
+
+    agent =  RLModels.NoSoftMax.RLLearner_basic(α_v, α_s, 0)
+    model_stim = RLModels.NoSoftMax.rl_learning_sr(sub1_env, agent, sub1_subinfo, dodecay = false)
+    mse = evaluate_relation(model_stim[:p_selection_history], sub1_subinfo.RT)[:MSE]
+    push!(v_list, α_v)
+    push!(s_list, α_s)
+    push!(mse_list, mse)
+end
+
+gradients_table = DataFrame(alpha_v = v_list, alpha_s = s_list, mse = mse_list)
+
+using Gadfly
+p = plot(gradients_table, x = :alpha_v, y = :alpha_s, color = :mse, Geom.point)
+q = plot(gradients_table, x = :mse, Geom.histogram)
--- tmp/generate_csvfile_for_lingwangs_model.R
+++ tmp/generate_csvfile_for_lingwangs_model.R
@@ -1,25 +1,56 @@
 library(tidyverse)
-baseline_margin <- read_csv("/Users/dddd1007/project2git/cognitive_control_model/ref_code/Lingwang_CCC/data/sub01.csv") %>%
-    select(block1,block2,block3,block4,block5,block6)
+library(fastDummies)
 all_data <- read_csv("/Users/dddd1007/project2git/cognitive_control_model/data/input/pure_all_data_transformed.csv")
 
-renamer <- function(input_dataframe, baseline_margin){
-    input_dataframe <- select(input_dataframe, stim_loc,Type,Response,RT, congruency, run, Trial, correct_action)
+renamer <- function(input_dataframe){
+    input_dataframe <- select(input_dataframe, stim_loc,Type,Response,RT, congruency, Trial, correct_action, prop, condition)
     input_dataframe <- input_dataframe %>%
         mutate(error_outlier = abs(Type - 1)) %>%
-        rename(stimPosit = stim_loc, correction = Type, respPosit = correct_action, nblock = run, hand = Response, trial_index = Trial)
+        rename(stimPosit = stim_loc, correction = Type, 
+               respPosit = correct_action, 
+               hand = Response, trial_index = Trial)
     postError <- c(0, input_dataframe$error_outlier)
     postError <- postError[-length(postError)]
-    baseline_margin = baseline_margin[1:nrow(input_dataframe),]
-
-    input_data <- cbind(input_dataframe, postError, baseline_margin)
+   
+    input_data <- cbind(input_dataframe, postError)
     
     return(input_data)
 }
 
-savepath = "/Users/dddd1007/project2git/cognitive_control_model/ref_code/Lingwang_CCC/data/wang_1a_input/"
-for (i in 28:36) {
+change_outlier <- function(input_dataframe){
+    RT_value <- input_dataframe$RT
+    sd_RT <- sd(RT_value)
+    mean_RT <- mean(RT_value)
+    outlier_data <- rep(0, nrow(input_dataframe))
+    index_outlier_3sd <- which(RT_value < mean_RT -3*sd_RT | RT_value > mean_RT + 3*sd_RT)
+    index_error <- which(input_dataframe$correction == 0)
+    outlier_data[index_outlier_3sd] <- 2
+    outlier_data[index_error] <- 1
+    
+    input_dataframe$error_outlier <- outlier_data
+    return(input_dataframe)
+}
+
+change_block_index <- function(input_dataframe) {
+    prop_value <- input_dataframe$prop
+    block_value <- rep(0, length(prop_value))
+    block_index <- 1
+    last_prop <- prop_value[1]
+    for (i in 1:length(prop_value)) {
+        if (last_prop != prop_value[i]) {
+            block_index <- block_index + 1
+            last_prop = prop_value[i]
+        }
+        block_value[i] <- block_index 
+    }
+    input_dataframe$nblock <- block_value
+    result_dataframe <- dummy_cols(input_dataframe, select_columns = "nblock")
+    return(result_dataframe)
+}
+
+savepath = "/Users/dddd1007/project2git/cognitive_control_model/ref_code/Lingwang_CCC/data/wang_model_input/"
+for (i in 1:18) {
     foo = filter(all_data, Subject_num == i)
-    bar = renamer(foo, baseline_margin)
+    bar = change_outlier(change_block_index(renamer(foo)))
     write_csv(bar, paste0(savepath, "sub_", i, "_prepared_data.csv"))
 }
