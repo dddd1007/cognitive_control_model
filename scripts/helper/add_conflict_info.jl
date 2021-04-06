e343ff3932c13551e55fdecd73ed22289d2fcad5
Xiaokai Xia <xia@xiaokai.me>
2021-04-06 11:02:15 +0800

    固定CCC对应的行为结果

--- scripts/helper/add_conflict_info.jl
+++ scripts/helper/add_conflict_info.jl
@@ -2,7 +2,7 @@ using Models, DataFrames, DataFramesMeta, CSV
 
 # prepare data
 raw_data = CSV.read("/Users/dddd1007/project2git/cognitive_control_model/data/input/pure_all_data.csv", DataFrame)
-raw_data = @where(raw_data, :Response .!= "NA")
+raw_data = @where(raw_data, :Type .== "hit")
 
 estimate_parameter = CSV.read("/Users/dddd1007/project2git/fmri_analysis2_nipype_type/data/inputs/behavioral_data/estimated_parameters_Wang_CCC.csv", DataFrame)
 include("/Users/dddd1007/project2git/cognitive_control_model/scripts/helper/import_all_data.jl")
@@ -19,7 +19,11 @@ optim_params_dict = Dict(:α_v => sub_optim_params.:alpha_v...,
                         :CCC => sub_optim_params.:ccc...,
                         :decay => sub_optim_params.:decay...)
 temp_result = model_recovery(sub_env, sub_info, optim_params_dict, model_type = :_2a1d1CCC)
-raw_sub_data.pe = temp_result[:prediction_error]
+raw_sub_data.p_value = temp_result[:p_selection_history]
+raw_sub_data.ll = temp_result[:options_weight_history][:,1]
+raw_sub_data.lr = temp_result[:options_weight_history][:,2]
+raw_sub_data.rl = temp_result[:options_weight_history][:,3]
+raw_sub_data.rr = temp_result[:options_weight_history][:,4]
 raw_sub_data.conflict_value = temp_result[:conflict_list]
 raw_sub_data[!, :CCC] .= optim_params_dict[:CCC]
 sub1_result_table = @transform(raw_sub_data, if_below_CCC = (-(sign.(:conflict_value - :CCC)) .- 1) ./ 2 .+ 1)
@@ -38,7 +42,11 @@ for sub_num in 2:36
                             :CCC => sub_optim_params.:ccc...,
                             :decay => sub_optim_params.:decay...)
     temp_result = model_recovery(sub_env, sub_info, optim_params_dict, model_type = :_2a1d1CCC)
-    raw_sub_data.pe = temp_result[:prediction_error]
+    raw_sub_data.p_value = temp_result[:p_selection_history]
+    raw_sub_data.ll = temp_result[:options_weight_history][:,1]
+    raw_sub_data.lr = temp_result[:options_weight_history][:,2]
+    raw_sub_data.rl = temp_result[:options_weight_history][:,3]
+    raw_sub_data.rr = temp_result[:options_weight_history][:,4]
     raw_sub_data.conflict_value = temp_result[:conflict_list]
     raw_sub_data[!, :CCC] .= optim_params_dict[:CCC]
     result_table = @transform(raw_sub_data, if_below_CCC = (-(sign.(:conflict_value - :CCC)) .- 1) ./ 2 .+ 1)
