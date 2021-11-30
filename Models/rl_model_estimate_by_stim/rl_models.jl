cf0f95446f5b5758adb92dbd0b4a5edc13f99131
Xiaokai Xia <xia@xiaokai.me>
2021-11-30 15:36:47 +0800

    根据研究思路修改文件结构

---
+++ Models/rl_model_estimate_by_stim/rl_models.jl
@@ -0,0 +1,23 @@
+## 编写各强化模型
+
+#= 模型分别为
+- ab
+- sr
+- sr_decay
+- ab_volatility
+- sr_volatility
+- sr_volatility_decay
+
+其中
+- ab : 抽象的刺激反应联结概念
+- sr : 具体的刺激反应联结概念
+- decay : 未激活的联结概念的概率衰减
+- volatilty : 考虑环境不稳定性分别估计学习率
+
+=#
+
+function ab_model(α::Float64, stim_feature_seq)
+    
+end 
+
+    
